3f509af431bcf4523a43ca1d6a47dd65d0794bdd
OscarMolina <molinaoscar159753@gmail.com>
2023-03-03 03:16:38 -0500

    actualizando

--- Ampay/Physics.cpp
+++ Ampay/Physics.cpp
@@ -160,7 +160,10 @@ inline void Physics::OnEnemyHit(Enemy* enemy)
 
  void Physics::OnPlayerHit(float damage)
 {
+	Player::GetInstance().ReduceHealth(damage);
 
+	if (Player::GetInstance().GetHealth() > 0)
+		Audio::GetInstance().PlaySound(Audio::GetInstance().GetSoundsMap().find("PlayerHit")->second);
 }
 
 bool Physics::PointInSphere(Camera& cam, glm::vec3& other, float radius)
