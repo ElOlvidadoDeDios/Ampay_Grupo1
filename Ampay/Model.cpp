6b9f0e547d0ddabf116a4f3ac667576a3f5a3f72
ConnieYbarguen <yerisconnie@gmail.com>
2023-03-03 04:15:37 -0500

    modificando y actulizando

--- Ampay/Model.cpp
+++ Ampay/Model.cpp
@@ -75,7 +75,7 @@ void Model::loadModel(std::string path)
 	{
 		printf("ERROR: Unable to load model\n");
 		return;
-	}
+	} 
 
 	directory = path.substr(0, path.find_last_of('/'));
 	processNode(scene->mRootNode, scene);
