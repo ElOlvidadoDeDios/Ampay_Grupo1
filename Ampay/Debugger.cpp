736f64df76fc4f286bbb414b8bab3a4f290e2555
ElOlvidadoDeDios <luisabertosanchezvalverde@gmail.com>
2023-03-03 01:10:24 -0500

    Debugger1

--- Ampay/Debugger.cpp
+++ Ampay/Debugger.cpp
@@ -40,15 +40,15 @@ void Debugger::DrawRay(glm::vec3& rayPos, glm::vec3& rayDir, Camera& cam)
 		(rayDir.x * 50.0f) + rayPos.x, (rayDir.y * 50.0f) + rayPos.y, (rayDir.z * 50.0f) + rayPos.z
 	};
 
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), &vertices[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), &vertices[0]);//Esta función actualiza una parte del búfer de vértices actual con nuevos datos de vértices. La función toma como entrada el tipo de búfer, un desplazamiento en bytes desde el inicio del búfer, el tamaño de los datos a actualizar y un puntero a los nuevos datos.
 
-	m_shader.SetMat4("view", cam.GetViewMatrix());
-	m_shader.SetMat4("projection", cam.GetProjectionMatrix());
-	m_shader.SetMat4("model", model);
+	m_shader.SetMat4("view", cam.GetViewMatrix());//Esta función establece el valor de una variable de matriz uniforme en el programa de sombreado actualmente activo
+	m_shader.SetMat4("projection", cam.GetProjectionMatrix());//Esta función establece el valor de una variable de matriz uniforme en el programa de sombreado actualmente activo
+	m_shader.SetMat4("model", model);//Esta función establece el valor de una variable de matriz uniforme en el programa de sombreado actualmente activo
 
-	glBindVertexArray(m_vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glBindVertexArray(0);
+	glBindVertexArray(m_vao);//Esta función enlaza el objeto de matriz de vértices actual con el identificador especificado
+	glDrawArrays(GL_TRIANGLES, 0, 3);//Esta función dibuja primitivas geométricas utilizando los datos del búfer de vértices actual
+	glBindVertexArray(0);//Esta función desenlaza el objeto de matriz de vértices actual.
 
-	m_shader.DeactivateProgram();
+	m_shader.DeactivateProgram();//Esta función desactiva el programa de sombreado actualmente activo, lo que permite que otros programas de sombreado se activen en su lugar.
 }
