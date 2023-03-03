f13ddee27c99de258925c8c478a4f58e8b3296ad
ConnieYbarguen <yerisconnie@gmail.com>
2023-03-03 04:06:35 -0500

    actualizado

--- Ampay/Framebuffer.cpp
+++ Ampay/Framebuffer.cpp
@@ -23,7 +23,7 @@ void Framebuffer::CreateFramebuffer()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_texture, 0);
-	glBindTexture(GL_TEXTURE_2D, 0)
+	glBindTexture(GL_TEXTURE_2D, 0);
 
 	// Create render buffer to attach stencil and depth data to it 
 	glGenRenderbuffers(1, &m_rbo);
