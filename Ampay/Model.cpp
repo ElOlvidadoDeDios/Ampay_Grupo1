e1ac8855f301d3ab481bce0e4949b5b873ae580d
ConnieYbarguen <yerisconnie@gmail.com>
2023-03-03 04:15:47 -0500

    modificando y actulizando

--- Ampay/Model.cpp
+++ Ampay/Model.cpp
@@ -91,7 +91,7 @@ void Model::processNode(aiNode * node, const aiScene * scene)
 	 
 	for (GLuint i = 0; i < node->mNumChildren; i++)
 	{
-		processNode(node->mChildren[i], scene);
+		processNode(node->mChildren[i], scene); 
 	}
 }
 
