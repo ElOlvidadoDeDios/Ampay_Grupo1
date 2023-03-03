463921cfe1952ec2551b55c7360c303a91bcd042
ConnieYbarguen <yerisconnie@gmail.com>
2023-03-03 04:07:13 -0500

    corregido

--- Ampay/Framebuffer.cpp
+++ Ampay/Framebuffer.cpp
@@ -35,8 +35,6 @@ void Framebuffer::CreateFramebuffer()
 	// Ensure that the frame buffer is complete and ready to be used
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 		printf("ERROR: Unable to create custom framebuffer."); 
-
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void Framebuffer::CreateShadowFramebuffer()
